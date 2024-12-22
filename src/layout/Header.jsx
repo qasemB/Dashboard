dcd94f459d68d9b5b3349085ce9e5e43681577e8
Kiana Heidari <kiana_hedari71@yahoo.com>
2024-12-22 00:20:42 -0500

    hreader

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from "../img/8439b51bb7a19f6e65ce1064bc37c197.svg"
 const Header = () => {
     return (
-        <div className='bg-gradient-to-r from-white via-blue-700 via-green-500 to-purple-500 fixed top-0 left-0 z-100  w-full h-16  bg-white  shadow-md'>
+        <div className='bg-gradient-to-r from-white via-blue-700  to-purple-500 fixed top-0 left-0 z-100  w-full h-16  bg-white  shadow-md'>
             <div className='fixed left-6 top-4'>
                 <img src={Logo} alt='logo'/>
             </div>
